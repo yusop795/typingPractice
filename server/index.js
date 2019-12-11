83d5972be7045873025fc8f31a55075e3b959414
ellapresso <ellapresso@gmail.com>
2019-12-11 19:54:17 +0900

    주석추가
    나중에 헷갈릴까봐 요약 달았음

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
 function rank(name, point) {
+  //이름과 점수를 넘겨받는다.
+  //rank.json에 있는 데이터와 넘겨받은 데이터를 비교하여 상위 10개를 다시 재저장(덮어쓰기)한다.
   fs.readFile("rank.json", "utf8", (err, data) => {
     data = JSON.parse(data);
     data.push({ name, point });
