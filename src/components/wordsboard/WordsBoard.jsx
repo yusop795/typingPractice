94aac7069098fe63a4b5c8b5dfe5d65873a22454
ellapresso <ellapresso@gmail.com>
2020-01-11 13:07:50 +0900

    타이머, 점수 임의 삽입

--- src/components/wordsboard/WordsBoard.jsx
+++ src/components/wordsboard/WordsBoard.jsx
@@ -5,7 +5,10 @@ function WordsBoard(words) {
   /* typing field에서 받은 랜덤 단어 = words */
   return (
     <ul className="board_container">
-      <div className="timer">00:00:00</div>
+      <div className="score_timer">
+        <span className="timer">남은 시간 00s</span>
+        <span className="score">현재 점수 : 10</span>
+      </div>
       {words.voca.map((d, i) => {
         return (
           <li key={i} place={d.place} className="board_item">
