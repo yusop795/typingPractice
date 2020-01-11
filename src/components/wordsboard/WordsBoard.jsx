136bacfa59644a545f09e569b4cb5109f92b64b8
ellapresso <ellapresso@gmail.com>
2020-01-11 13:01:44 +0900

    타이머 임의 추가

--- src/components/wordsboard/WordsBoard.jsx
+++ src/components/wordsboard/WordsBoard.jsx
@@ -5,6 +5,7 @@ function WordsBoard(words) {
   /* typing field에서 받은 랜덤 단어 = words */
   return (
     <ul className="board_container">
+      <div className="timer">00:00:00</div>
       {words.voca.map((d, i) => {
         return (
           <li key={i} place={d.place} className="board_item">
