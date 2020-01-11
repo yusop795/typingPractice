2af1b8c4234ced3cb74f9be9904934aececcd7c0
ellapresso <ellapresso@gmail.com>
2020-01-11 12:55:01 +0900

    score추가, 랭킹에 나타나는 이름 뒤 숫자 제거

--- src/components/ranking/Ranking.jsx
+++ src/components/ranking/Ranking.jsx
@@ -8,9 +8,10 @@ function Ranking(ranking) {
       <strong>Ranking</strong>
       <ul className="rank_list">
         {ranking.rankers.map((v, i) => {
+          const div = v.name.indexOf('#');
           return (
             <li key={i}>
-              <div className="ranker_name">{v.name}</div>
+              <div className="ranker_name">{v.name.substr(0,div)}</div>
               <div className="ranker_point">{v.point}</div>
             </li>
           );
