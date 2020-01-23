167b994391313dac7befbfe978b7e476f45e04ca
yusop <yusop795@gmail.com>
2020-01-23 18:13:41 +0900

    intermediate saving

--- src/components/wordsboard/WordsBoard.jsx
+++ src/components/wordsboard/WordsBoard.jsx
@@ -5,10 +5,6 @@ function WordsBoard(words) {
   /* typing field에서 받은 랜덤 단어 = words */
   return (
     <ul className="board_container">
-      <div className="score_timer">
-        <span className="timer">남은 시간 00s</span>
-        <span className="score">현재 점수 : 10</span>
-      </div>
       {words.voca.map((d, i) => {
         return (
           <li key={i} place={d.place} className="board_item">
--- src/containers/TypingField.jsx
+++ src/containers/TypingField.jsx
@@ -8,6 +8,13 @@ import "./containers.scss";
 /*key, q는 필수값... q=> 검색어... 랜덤은 x*/
 
 class TypingField extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      timer: 0,
+      timerFunc: {}
+    };
+  }
   componentDidMount() {
     /* 해당 컴포넌트 초기값 세팅 될 때 실행됨 => saga call (saga/index.js) INITIAL_WORDS 을 키로 실행시킬 function 을 찾는다*/
     this.props.actionReducerCall({
@@ -16,6 +23,30 @@ class TypingField extends React.Component {
       name: "on_word"
     });
   }
+  startTimer(time) {
+    console.log("start".time);
+    this.setState({
+      timer: time
+    });
+    // this.state.timerFunc = setInterval( decrementTime(), 1000);
+    const timerfunc = () => {
+      let timer_start = this.state.timer;
+      const set = document.querySelector(".show_num");
+      console.log("timer_start", timer_start);
+      set.innerText = timer_start;
+      if (timer_start > 0) {
+        timer_start--;
+        this.setState({
+          timer: timer_start
+        });
+      } else {
+        clearInterval(timer_starting);
+      }
+    };
+
+    const timer_starting = setInterval(timerfunc(), 1000);
+  }
+  decrementTime() {}
   render() {
     // console.log("rendom words ", this.props.on_word);
     // const { point, name } = this.props.data;
@@ -23,6 +54,13 @@ class TypingField extends React.Component {
       <div>
         <div className="container">
           <h1>GAME START!</h1>
+          <div className="score_timer">
+            <span className="timer">
+              남은 시간{" "}
+              <span className="show_num">{() => this.startTimer(30)}</span>s
+            </span>
+            <span className="score">현재 점수 : 10</span>
+          </div>
           <div className="top_container">
             <WordsBoard voca={this.props.on_word} />
             <Ranking rankers={this.props.data} />
